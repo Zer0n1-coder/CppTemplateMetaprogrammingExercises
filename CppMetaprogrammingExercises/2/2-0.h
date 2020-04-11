66582cc6c25941f343e57805a77bc0565d7ec01f
cherui <cherui_work@outlook.com>
2020-04-11 09:01:30 +0800

    2-0

---
+++ CppMetaprogrammingExercises/2/2-0.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <type_traits>
+
+template<bool Is_Ref, class T>
+struct _add_const_ref
+{
+	using type = T;
+};
+
+template<class T>
+struct _add_const_ref<true, T>
+{
+	using type = T;
+};
+
+template<class T>
+struct _add_const_ref<false, T>
+{
+	using type = const T&;
+};
+
+template<class T>
+struct add_const_ref:_add_const_ref<std::is_reference_v<T>,T>
+{
+};
+
+//test
+static void test()
+{
+	static_assert(std::is_same_v<const int&, add_const_ref<int>::type>);		//success
+	static_assert(std::is_same_v<const int&, add_const_ref<int&>::type>);		//fail
+	static_assert(std::is_same_v<const int&, add_const_ref<const int&>::type>); //success
+	static_assert(std::is_same_v<int&, add_const_ref<int&>::type>);				//success
+	static_assert(std::is_same_v<const int&, add_const_ref<const int>::type>);	//success
+}
+
+
+
