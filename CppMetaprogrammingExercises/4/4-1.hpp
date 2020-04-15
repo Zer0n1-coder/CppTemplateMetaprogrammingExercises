94482bd345dd20574492bb05cca1c572f880da52
CheRui <850989926@qq.com>
2020-04-15 10:10:10 +0800

    4-1

---
+++ CppMetaprogrammingExercises/4/4-1.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <type_traits>
+
+template<class V1,class V2>
+struct logical_and
+{
+	static const bool value = V1::value && V2::value;
+};
+
+template<class V1, class V2>
+struct logical_or
+{
+	static const bool value = V1::value || V2::value;
+};
+
+
+
+void test4_1()
+{
+	static_assert(logical_and<std::is_reference<int&>, std::is_reference<int>>::value);		//fail
+	static_assert(logical_or<std::is_reference<int&>, std::is_reference<int>>::value);		//success
+	static_assert(logical_and<std::is_reference<int&>, std::is_reference<int&>>::value);	//success
+	static_assert(logical_or<std::is_reference<int&>, std::is_reference<int&>>::value);		//success
+	static_assert(logical_or<std::is_reference<int>, std::is_reference<int>>::value);		//fail
+}
+
+
