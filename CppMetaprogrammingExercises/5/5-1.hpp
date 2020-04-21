808f55ebfca4fcffb93cd05f60b396469bfda0e8
CheRui <850989926@qq.com>
2020-04-21 17:29:50 +0800

    update 5-1

--- CppMetaprogrammingExercises/5/5-1.hpp
+++ CppMetaprogrammingExercises/5/5-1.hpp
@@ -1,19 +1,18 @@
 #pragma once
-#include <limits>
 #include "5-0.hpp"
 
-template<class T, class T2, int begin, int end, int flag>
+template<class Out_Tiny, class In_Tiny, int begin, int end, int flag>
 struct Operator
 {
-	static constexpr int value = begin <= flag ? at_<T2, int_constant<begin>>::type::value * 2 : at_<T2, int_constant<begin>>::type::value;
-	using type = typename Operator<tiny_push_back<T, int_constant<value>>::type, T2, begin + 1, end, flag>::type;
+	static constexpr int value = begin <= flag ? at_<In_Tiny, int_constant<begin>>::type::value * 2 : at_<In_Tiny, int_constant<begin>>::type::value;
+	using type = typename Operator<tiny_push_back<Out_Tiny, int_constant<value>>::type, In_Tiny, begin + 1, end, flag>::type;
 };
 
-template<class T, class T2, int end, int flag>
-struct Operator<T, T2, end, end, flag>
+template<class Out_Tiny, class In_Tiny, int end, int flag>
+struct Operator<Out_Tiny, In_Tiny, end, end, flag>
 {
-	static constexpr int value = end <= flag ? at_<T2, int_constant<end>>::type::value * 2 : at_<T2, int_constant<end>>::type::value;
-	using type = tiny_push_back<T, int_constant<value>>::type
+	static constexpr int value = end <= flag ? at_<In_Tiny, int_constant<end>>::type::value * 2 : at_<In_Tiny, int_constant<end>>::type::value;
+	using type = tiny_push_back<Out_Tiny, int_constant<value>>::type;
 };
 
 void test5_1()
